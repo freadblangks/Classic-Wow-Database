aa3098abf82456351c4b43147155c433724709ed
gamemechanicwow <stesve@gmail.com>
2021-04-21 06:08:55 +0200

    use mysqli

--- includes/db.php
+++ includes/db.php
@@ -9,14 +9,14 @@ require_once('includes/DbSimple/Generic.php');
 global $AoWoWconf;
 
 // Подключение к БД мангос (версия 3 :) )
-$DB = DbSimple_Generic::connect("mysql://".$AoWoWconf['mangos']['user'].":".$AoWoWconf['mangos']['pass']."@".$AoWoWconf['mangos']['host']."/".$AoWoWconf['mangos']['db']);
+$DB = DbSimple_Generic::connect("mysqli://".$AoWoWconf['mangos']['user'].":".$AoWoWconf['mangos']['pass']."@".$AoWoWconf['mangos']['host']."/".$AoWoWconf['mangos']['db']);
 $DB->setErrorHandler('databaseErrorHandler');
 $DB->setIdentPrefix($AoWoWconf['mangos']['aowow']);
 $DB->query('SET NAMES ?', 'utf8');
 // Подключение к БД realmd
 if($AoWoWconf['realmd'])
 {
-	$rDB = DbSimple_Generic::connect("mysql://".$AoWoWconf['realmd']['user'].":".$AoWoWconf['realmd']['pass']."@".$AoWoWconf['realmd']['host']."/".$AoWoWconf['realmd']['db']);
+	$rDB = DbSimple_Generic::connect("mysqli://".$AoWoWconf['realmd']['user'].":".$AoWoWconf['realmd']['pass']."@".$AoWoWconf['realmd']['host']."/".$AoWoWconf['realmd']['db']);
 	$rDB->setErrorHandler('databaseErrorHandler');
 	$rDB->query('SET NAMES ?', 'utf8');
 }
